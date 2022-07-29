e4dee89f98721cd3dcbe39b6d98f3366d9fe3a58
giovannyalbarracin <giovanny_albarracin@epam.com>
2022-07-28 20:09:37 -0500

    adding task 4.1

---
+++ db/seed.sql
@@ -0,0 +1,20 @@
+insert into product(description, price, title) values
+  ('Short Product Description1', 2.4, 'ProductOne'),
+  ('Short Product Description3', 10, 'ProductNew'),
+  ('Short Product Description2', 23, 'ProductTop'),
+  ('Short Product Description7', 15, 'ProductTitle'),
+  ('Short Product Description2', 23, 'Product'),
+  ( 'Short Product Description4', 15, 'ProductTest'),
+  ('Short Product Descriptio1', 23, 'Product2'),
+  ('Short Product Description7', 15, 'ProductName')
+
+
+insert into stock (product_id, count) values
+('bdfcc42f-2382-402c-a0f7-d57b3a3d4523', 4),
+('8ef738c2-245a-47cf-8233-ac4c7bd05b6b', 6),
+('432fc213-9441-4363-add5-2ae52407a08c', 7),
+('15f5f737-ce51-44ef-b9e7-127960868879', 12),
+('9d9012f8-a97a-442b-9612-1132595ee7c6', 7),
+('4aa627b3-966c-44ec-a4aa-3dfe9bdb6c49', 8),
+('ca092204-253f-4d42-baab-78b020927d62', 2),
+('036271b0-0531-4e93-9880-db43ac80793a', 3)
--- handler.test.js
+++ handler.test.js
@@ -1,14 +1,14 @@
-const getProductList = require('./handlers/getProductList');
-const getProductById = require('./handlers/getProductById');
+const productList = require('./handlers/getProductList');
+const productById = require('./handlers/getProductById');
 const products = require('./data/products.json')
 describe('product api test', () => {
   it('get ProductList working', async() => {
-    const productList = await getProductList();
+    const productList = await productList();
     expect(productList.body).toBe(JSON.stringify(products));
   });
 
   it('get Produc by id working', async() => {
-    const product = await getProductById('7567ec4b-b10c-48c5-9345-fc73c48a80aa');
+    const product = await productById('7567ec4b-b10c-48c5-9345-fc73c48a80aa');
     expect(product.body).toBe(JSON.stringify(products[0]));
   });
 })
